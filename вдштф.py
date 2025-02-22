1ce0a14772fa7e39809f46f997a002958ad70431
egorbazanow <egorbazanow@bk.ru>
2025-02-22 07:35:58 +0700

    done

--- вдштф.py
+++ вдштф.py
@@ -11,11 +11,11 @@ def vanderwal(V,T):
 V = np.linspace(b+10**-5, 10**-3, 1000)
 prom = (0.00013589-7.16308308e-05)/1000
 def f1(V):
-    P = (R*(-130+273.15)/(V-b)) - prom/(V**2)
+    P = (R*(-130+273.15)/(V-b)) - a/(V**2)
     return P
 lenght = 0
 for i in range(999):
     V = prom*i + (7.16308308e-05)
-    bet = (((f1(V)-f1(V+prom))**2)+prom**2)**(0.5)
+    bet = (((vanderwal(V, temp_klvn)-vanderwal(V +prom, temp_klvn))**2)+prom**2)**(0.5)
     lenght = lenght+bet
 print(lenght)
